23833b35a0219dc10ee122f9a7e0d41a370af220
rpatil5 <rishistar05@gmail.com>
2024-12-23 21:12:34 -0600

    Initial commit

---
+++ popup.js
@@ -0,0 +1,6 @@
+// chrome.identity.getProfileUserInfo({'accountStatus': 'ANY'},function(userInfo) {
+//     console.log(userInfo.email);
+//     console.log(userInfo.id);
+//     console.log(userInfo.idToken);
+//     document.getElementById('email').innerText = userInfo.id;
+// })
